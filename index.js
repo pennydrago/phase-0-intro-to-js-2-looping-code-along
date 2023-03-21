88a1c19ecabed8f99338aebadbd7d5ee12097a15
Penelope Drago <prd@drago.com.au>
2023-03-21 17:31:08 +1100

    add functions attempt 2

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Code your solutions in this file
+
+// Function to write cards
+
+function writeCards(name, event) {
+  const message = [];
+  for (let i = 0; i < name.length; i++) {
+    message.push(`Thank you, ${name[i]}, for the wonderful ${event} gift!`);
+  }
+  return message;
+}
+
+// Function to count down to zero
+
+function countDown(number) {
+  while (number >= 0) {
+    console.log(number--);
+  }
+}
